8436dda2b4ac08eaed3a7f37f4406d8e6b903ed3
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2022-04-25 17:36:09 +0900

    Revert "Laravel 8 Tutorial 3 Routing with Examples"
    This reverts commit 963a755a614ec37a9d09a485ba5430c919ec6692.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,20 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::view('/test', 'test');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-Route::get('/test', [TestController::class, 'index']);
-
-// Route::get('/user/{id}', function ($id) {
-//     return 'User' . $id;
-// });
-
-Route::get('/user/{name?}', function ($name = null) {
-    return 'User' . $name;
-});
-
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// })->middleware(['auth'])->name('dashboard');
-
-// require __DIR__.'/auth.php';
+require __DIR__.'/auth.php';
