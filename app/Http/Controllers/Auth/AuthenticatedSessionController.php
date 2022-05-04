b1ed8f39f4645c98c0febba92346915065e903a5
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2022-05-04 20:26:10 +0900

    Revert "save"
    This reverts commit 975af230ac645bdc90f33c26bb8681848675faca.

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -43,12 +43,12 @@ class AuthenticatedSessionController extends Controller
      */
     public function destroy(Request $request)
     {
-        Auth::guard('user')->logout();
+        Auth::guard('web')->logout();
 
         $request->session()->invalidate();
 
         $request->session()->regenerateToken();
 
-        return redirect('/login');
+        return redirect('/');
     }
 }
