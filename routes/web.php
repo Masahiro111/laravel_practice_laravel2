b9f27752910dc28fb76f4f352bc5d02096e240fd
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2022-05-12 17:50:42 +0900

    777

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HelloController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,4 +22,7 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+Route::get('/hello', [HelloController::class, 'index'])
+    ->name('hello.index');
+
+require __DIR__ . '/auth.php';
