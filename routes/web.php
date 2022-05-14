4799d4dc0e72be0d9e73983566536fb409f2be52
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2022-05-14 16:28:09 +0900

    Revert "777"
    This reverts commit b9f27752910dc28fb76f4f352bc5d02096e240fd.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\HelloController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,7 +21,4 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-Route::get('/hello', [HelloController::class, 'index'])
-    ->name('hello.index');
-
-require __DIR__ . '/auth.php';
+require __DIR__.'/auth.php';
