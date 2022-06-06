dc4ea6663f306ff3b2df7b6d6968a02d8d497259
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2022-06-06 19:28:45 +0900

    create blade components

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,34 +1,3 @@
-@props([
-'message' => 'これは初期メッセージです。',
-'type' => 'success',
-])
-
-@php
-function addClass($type){
-$result = '';
-switch($type){
-case 'success' :
-$result = 'bg-green-500';
-break;
-case 'info' :
-$result = 'bg-blue-500';
-break;
-case 'alert' :
-$result = 'bg-red-500';
-break;
-case 'warning' :
-$result = 'bg-yellow-500';
-break;
-defalut:
-$result = 'bg-green-200';
-}
-return $result;
-}
-@endphp
-
-<div
-     {{ $attributes->merge([
-    'class'=>'flex justify-between p-4 items-center text-white ' . addClass($type) . ' text-white']) }}>
-    <div>{{ $message }}</div>
-    <button>x</button>
+<div>
+    <!-- Happiness is not something readymade. It comes from your own actions. - Dalai Lama -->
 </div>
