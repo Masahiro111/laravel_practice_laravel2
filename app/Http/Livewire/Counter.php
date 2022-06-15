07f929693e7569bc79d6bca85cd39979a33b0c89
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2022-06-16 03:40:44 +0900

    save

--- app/Http/Livewire/Counter.php
+++ app/Http/Livewire/Counter.php
@@ -2,16 +2,35 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\User;
 use Livewire\Component;
 
 class Counter extends Component
 {
 
+    public $users;
     public $count = 10;
+    public $message = 'message';
+
+    public function mount()
+    {
+        $this->users = User::all();
+    }
 
     public function increment()
     {
         $this->count++;
+    }
+
+    public function delUser(int $id)
+    {
+        $this->users = $this->users->filter(function ($value, $key) use ($id) {
+            return $value['id']  != $id;
+        });
+
+        $user = User::find($id);
+
+        $user->delete();
     }
 
     public function render()
--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,4 +1,38 @@
 <div>
+
+    <div>
+        <h2>ユーザ一覧</h2>
+        <ul>
+            @foreach($users as $user)
+            <li>
+                {{ $user->name }} <button wire:click="delUser({{ $user->id }})">削除</button>
+            </li>
+            @endforeach
+        </ul>
+    </div>
+
     <h2>{{ $count }}</h2>
     <p><button wire:click="increment">+1</button></p>
+
+    <hr>
+
+    <p>debounce 500ms</p>
+    <input
+           type="text"
+           wire:model.debounce.500ms="message">
+
+    <hr>
+
+    <p>lazy</p>
+    <input
+           type="text"
+           wire:model.lazy="message">
+
+    @if(!$message)
+    <p style="color:red;font-weight:bold">文字を入力してください。</p>
+    @else
+    <p>文字を入力しました。</p>
+    @endif
+
+    {{ $message }}
 </div>
