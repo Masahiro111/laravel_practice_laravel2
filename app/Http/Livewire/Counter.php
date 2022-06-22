1e0876f703048d4878fd246112685363c959b059
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2022-06-22 18:47:14 +0900

    Revert "save"
    This reverts commit 07f929693e7569bc79d6bca85cd39979a33b0c89.

--- app/Http/Livewire/Counter.php
+++ app/Http/Livewire/Counter.php
@@ -2,35 +2,16 @@
 
 namespace App\Http\Livewire;
 
-use App\Models\User;
 use Livewire\Component;
 
 class Counter extends Component
 {
 
-    public $users;
     public $count = 10;
-    public $message = 'message';
-
-    public function mount()
-    {
-        $this->users = User::all();
-    }
 
     public function increment()
     {
         $this->count++;
-    }
-
-    public function delUser(int $id)
-    {
-        $this->users = $this->users->filter(function ($value, $key) use ($id) {
-            return $value['id']  != $id;
-        });
-
-        $user = User::find($id);
-
-        $user->delete();
     }
 
     public function render()
