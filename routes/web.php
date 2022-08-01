07f0825a9963a4ed6277bf814176168106b7970d
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2022-08-01 14:54:45 +0900

    Revert "wip"
    This reverts commit 6d4850d87b8c1d0315a7d14768d7d33789e8fbed.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,18 +21,4 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-Route::prefix('products')
-    ->controller(ProductController::class)
-    ->as('products.')
-    ->group(function () {
-        Route::get('/', 'index')->name('index');
-        Route::get('/create', 'create')->name('create');
-        Route::post('/', 'store')->name('store');
-        Route::get('/{product}', 'show')->name('show');
-        Route::get('/{product}/edit', 'edit')->name('edit');
-        Route::put('/{product}', 'update')->name('update');
-        Route::delete('/{product}', 'destroy')->name('destroy');
-    });
-
-
-require __DIR__ . '/auth.php';
+require __DIR__.'/auth.php';
